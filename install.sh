88aac6614cb220a218b82eb219f2e36bb89e651b
mayaheartsu <136956080+mayaheartsu@users.noreply.github.com>
2023-08-24 09:53:40 -0600

    semi-functional install script implemented
    this script was previously a loose collection of notes and steps needed to be taken during/after setup. after snagging snippets of code from ArchTitus, most of this is handled via script now..

--- install.sh
+++ install.sh
@@ -1,31 +1,77 @@
 #!/bin/sh
 
-#packages
+echo -ne "
+-------------------------------------------------------------------------
+                    mayadots installer 
+-------------------------------------------------------------------------
+"
+
+echo -ne "
+-------------------------------------------------------------------------
+                    rate-mirrors installation & mirror sort
+-------------------------------------------------------------------------
+"
+yay -S rate-mirrors-bin
+sudo rate-mirrors --allow-root --protocol https arch | sudo tee /etc/pacman.d/mirrorlist
+
+echo -ne "
+-------------------------------------------------------------------------
+                    cpu microcode
+-------------------------------------------------------------------------
+"
+# determine processor type and install microcode
+proc_type=$(lscpu)
+if grep -E "GenuineIntel" <<< ${proc_type}; then
+    echo "Installing Intel microcode"
+    pacman -S --noconfirm --needed intel-ucode
+    proc_ucode=intel-ucode.img
+elif grep -E "AuthenticAMD" <<< ${proc_type}; then
+    echo "Installing AMD microcode"
+    pacman -S --noconfirm --needed amd-ucode
+    proc_ucode=amd-ucode.img
+fi
+
+echo -ne "
+-------------------------------------------------------------------------
+                    package installation
+-------------------------------------------------------------------------
+"
 yay -S afetch blueman base base-devel blueman bluez-tools brillo btop catppuccin-gtk-theme-mocha dunst file-roller firefox geeqie git gparted grim guvcview hyprland-git kitty libnotify light lightly-qt man-db mpv nano nemo nemo-fileroller neofetch nwg-look-bin obs-studio pamixer pavucontrol polkit polkit-kde-agent python-requests qogir-icon-theme qt5ct ranger rate-mirrors-bin rofi rofi-emoji sddm-catppuccin-git sddm-config-editor-git sddm-git slurp swappy swaybg swayidle swaylock-effects-git timeshift-bin tldr ttf-font-awesome vscodium-bin waybar webcord-bin wf-recorder wlr-randr xdg-desktop-portal-hyprland xdg-user-dirs xed
 
-#nvidia specific packages
-#nvidia-dkms or nvidia-open-dkms nvidia-settings nvidia-utils
+echo -ne "
+-------------------------------------------------------------------------
+                    graphics drivers
+-------------------------------------------------------------------------
+"
+# Graphics Drivers find and install
+gpu_type=$(lspci)
+if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
+    pacman -S --noconfirm --needed nvidia-dkms nvidia-settings nvidia-utils
+elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
+    pacman -S --noconfirm --needed xf86-video-amdgpu
+elif grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
+    pacman -S --noconfirm --needed libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
+elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; then
+    pacman -S --needed --noconfirm libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
+fi
+
+echo -ne "
+-------------------------------------------------------------------------
+                    enable sddm & set QT_QPA_PLATFORMTHEME
+-------------------------------------------------------------------------
+"
+sudo systemctl enable sddm.service
+echo "QT_QPA_PLATFORMTHEME=qt5ct" | sudo cat >> /etc/environment
+
+echo -ne "
+-------------------------------------------------------------------------
+                    DONE >:3
+-------------------------------------------------------------------------
+"
 
 #game related packages
 #lutris steam gamescope goverlay mangohud protonup-qt prismlauncher-bin
 
-#-10 dB on mic
-
-#https://github.com/yokoffing/Betterfox
-
-#rate-mirrors --allow-root --protocol https arch | sudo tee /etc/pacman.d/mirrorlist
 
 #/etc/environment
 #QT_QPA_PLATFORMTHEME=qt5ct MANGOHUD=1
-
-#intel-gpu-tools intel-media-driver libva-utils xf86-video-intel auto-cpufreq-git blueman bluez-tools pamixer pavucontrol hyprland waybar-hyprland rofi rofi-emoji dunst kitty swaybg swaylock-fancy-git swayidle light brillo btop file-roller pcmanfm ranger lightly-qt catppuccin-gtk-theme-mocha nwg-look-bin qt5ct polkit polkit-kde-agent rate-mirrors sddm-git sddm-catppuccin-git vscodium-bin webcord xdg-desktop-portal-hyprland xdg-user-dirs noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-font-awesome python-requests qogir-icon-theme libnotify grim slurp timeshift-bin gparted xed
-
-#systemctl enable sddm.service
-
-#https://github.com/1amSimp1e/dots/tree/late-night-%F0%9F%8C%83
-#https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.ziphttps://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Iosevka.zip
-#https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Iosevka.zip
-#https://github.com/googlefonts/noto-emoji/raw/main/fonts/NotoColorEmoji.ttf
-#https://github.com/microsoft/vscode-codicons/raw/main/dist/codicon.ttf
-
-#fc-cache -fv
