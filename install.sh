d1cfefea142edeb81ad45d0ca96b3e8c41e1450d
mayaheartsu <136956080+mayaheartsu@users.noreply.github.com>
2023-10-06 18:08:15 -0600

    Update install.sh
    add sudo to gpu driver install
    add keepassxc to package installation

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ echo -ne "
                     rate-mirrors installation & mirror sort
 -------------------------------------------------------------------------
 "
-yay -S rate-mirrors-bin
+yay -S --noconfirm rate-mirrors-bin
 sudo rate-mirrors --allow-root --protocol https arch | sudo tee /etc/pacman.d/mirrorlist
 
 echo -ne "
@@ -36,7 +36,7 @@ echo -ne "
                     package installation
 -------------------------------------------------------------------------
 "
-yay -S --noconfirm afetch blueman base base-devel blueman bluez-tools brillo btop catppuccin-gtk-theme-mocha dunst file-roller firefox geeqie git gparted grim guvcview hyprland-git kitty libnotify light lightly-qt man-db mpv nano nemo nemo-fileroller neofetch nwg-look-bin obs-studio pamixer pavucontrol pipewire pipewire-alsa pipewire-audio pipewire-pulse pipewire-jack polkit polkit-kde-agent python-requests qogir-icon-theme qt5ct ranger rate-mirrors-bin rofi rofi-emoji sddm-catppuccin-git sddm-config-editor-git sddm-git slurp swappy swaybg swayidle swaylock-effects-git timeshift-bin tldr ttf-font-awesome vscodium-bin waybar webcord-bin wf-recorder wireplumber wlr-randr xdg-desktop-portal-hyprland xdg-user-dirs xed
+yay -S --noconfirm afetch blueman base base-devel blueman bluez-tools brillo btop catppuccin-gtk-theme-mocha dunst file-roller firefox geeqie git gparted grim guvcview hyprland-git keepassxc kitty libnotify light lightly-qt man-db mpv nano nemo nemo-fileroller neofetch nwg-look-bin obs-studio pamixer pavucontrol pipewire pipewire-alsa pipewire-audio pipewire-pulse pipewire-jack polkit polkit-kde-agent python-requests qogir-icon-theme qt5ct ranger rate-mirrors-bin rofi rofi-emoji sddm-catppuccin-git sddm-config-editor-git sddm-git slurp swappy swaybg swayidle swaylock-effects-git timeshift-bin tldr ttf-font-awesome vscodium-bin waybar wf-recorder wget wireplumber wlr-randr xdg-desktop-portal-hyprland xdg-user-dirs xed
 
 echo -ne "
 -------------------------------------------------------------------------
@@ -46,13 +46,13 @@ echo -ne "
 # Graphics Drivers find and install
 gpu_type=$(lspci)
 if grep -E "NVIDIA|GeForce" <<< ${gpu_type}; then
-    pacman -S --noconfirm --needed nvidia-dkms nvidia-settings nvidia-utils
+   sudo pacman -S --noconfirm --needed nvidia-dkms nvidia-settings nvidia-utils
 elif lspci | grep 'VGA' | grep -E "Radeon|AMD"; then
-    pacman -S --noconfirm --needed xf86-video-amdgpu
+  sudo  pacman -S --noconfirm --needed xf86-video-amdgpu
 elif grep -E "Integrated Graphics Controller" <<< ${gpu_type}; then
-    pacman -S --noconfirm --needed libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
+  sudo  pacman -S --noconfirm --needed libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
 elif grep -E "Intel Corporation UHD" <<< ${gpu_type}; then
-    pacman -S --needed --noconfirm libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
+   sudo pacman -S --needed --noconfirm libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa
 fi
 
 echo -ne "
@@ -73,6 +73,8 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsM
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Iosevka.zip -P ~/Downloads
 wget https://github.com/googlefonts/noto-emoji/raw/main/fonts/NotoColorEmoji.ttf -P ~/Downloads
 wget https://github.com/microsoft/vscode-codicons/raw/main/dist/codicon.ttf -P ~/Downloads
+mkdir ~/.local
+mkdir ~/.local/share
 mkdir ~/.local/share/fonts
 unzip ~/Downloads/JetBrainsMono.zip -d ~/.local/share/fonts
 unzip ~/Downloads/Iosevka.zip -d ~/.local/share/fonts
@@ -86,8 +88,8 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 
-#game related packages
-#lutris steam gamescope goverlay mangohud protonup-qt prismlauncher-bin
+#optional game related packages
+#lutris steam gamemode gamescope goverlay mangohud protonup-qt prismlauncher-bin
 
 
 #/etc/environment
