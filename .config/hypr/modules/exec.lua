ea4fdfcff05e305006ded41997000cb819073636
maya <mayaephemera@proton.me>
2026-07-20 11:35:06 -0600

    add noctalia v5

--- .config/hypr/modules/exec.lua
+++ .config/hypr/modules/exec.lua
@@ -8,13 +8,16 @@
 -- https://wiki.hypr.land/Configuring/Basics/Dispatchers/#executing-with-rules
 
  hl.on("hyprland.start", function () 
-   hl.exec_cmd("qs -c noctalia-shell")
+   -- hl.exec_cmd("qs -c noctalia-shell")
+   hl.exec_cmd("noctalia")
    hl.exec_cmd("systemctl --user start hyprpolkitagent")
    hl.exec_cmd("hyprlauncher -d")
-   hl.exec_cmd("dunst")
+  -- hl.exec_cmd("dunst")
    hl.exec_cmd("clipse -listen")
    hl.exec_cmd("hyprpaper")
    hl.exec_cmd("fcitx5 -D")
    hl.exec_cmd("hypridle")
    hl.exec_cmd("blueman-applet")
+   hl.exec_cmd("dbus-update-activation-environment --systemd --all")
+   hl.exec_cmd("systemctl --user import-environment QT_QPA_PLATFORMTHEME")
  end)
