3bc44b97edfeacfe485c0f9db84b5304a476cf3c
maya <mayaephemera@proton.me>
2026-08-03 00:50:50 -0600

    unfuck hdr (finally)
    disable direct_scanout,
    set sdr_eotf and cm_sdr_eotf for hdr in browsers
    switch to hdr from hdredid
    
    all of this to ensure hdr swapchain isn't interfered with in games/videos

--- .config/hypr/modules/monitors.lua
+++ .config/hypr/modules/monitors.lua
@@ -15,16 +15,17 @@ hl.monitor({
     position = "2560x0",
     scale = 1,
     bitdepth = 10,
-    cm = "hdredid",
+    cm = "hdr",
     vrr = 3,  -- 1 = Always on, 2 = fullscreen only, 3 = only game or video
     supports_wide_color = 1,
     supports_hdr = 1,
-    sdrbrightness = 1.0,
+    sdrbrightness = 1.0, -- Think of this variable as a multiplier that applies after sdr_max_luminance. Keep it simple and keep at 1. Make more finite adjustments via sdr_max_luminance instead.
     sdrsaturation = 1.0,
     sdr_min_luminance = 0.0005,  -- Allow true black in SDR mode
-    sdr_max_luminance = 400,
+    sdr_max_luminance = 300, -- BT.2408 target is 203 nits SDR, 1000 nits HDR. Macbook Pro M1 is 500 nits SDR, 1000 - 1600 nits HDR. Macbook Pro M3 and beyond are 600 - 1000 nits SDR, 1000 - 1600 nits HDR. Steam Deck OLED is 600 nits SDR, 1000 nits HDR. All listed SDR values area peak values, likely half of that brightness during regular use. Go with your heart and don't melt your eyes
     min_luminance = 0.0005,
     max_luminance = 880,
+    sdr_eotf = "auto",
 })
 
 -- Left Monitor
@@ -34,24 +35,26 @@ hl.monitor({
     position = "0x0",
     scale = 1,
     bitdepth = 10,
-    cm = "hdredid",
+    cm = "hdr",
     vrr = 3,  -- 1 = Always on, 2 = fullscreen only, 3 = only game or video
     supports_wide_color = 1,
     supports_hdr = 1,
-    sdrbrightness = 1.0,
+    sdrbrightness = 1.0, -- Think of this variable as a multiplier that applies after sdr_max_luminance. Keep it simple and keep at 1.0. Make more finite adjustments via sdr_max_luminance instead
     sdrsaturation = 1.0,
     sdr_min_luminance = 0.0005,  -- Allow true black in SDR mode
-    sdr_max_luminance = 400,
+    sdr_max_luminance = 300, -- BT.2408 target is 203 nits SDR, 1000 nits HDR. Macbook Pro M1 is 500 nits SDR, 1000 - 1600 nits HDR. Macbook Pro M3 and beyond are 600 - 1000 nits SDR, 1000 - 1600 nits HDR. Steam Deck OLED is 600 nits SDR, 1000 nits HDR. All listed SDR values area peak values, likely half of that brightness during regular use. Go with your heart and don't melt your eyes
     min_luminance = 0.0005,
     max_luminance = 880,
+    sdr_eotf = "auto",
 })
 
 -- HDR rendering settings
 hl.config({
     render = ({
-        direct_scanout = 2, -- Enables direct scanout. Direct scanout attempts to reduce lag when there is only one fullscreen application on a screen (e.g. game). It is also recommended to set this to false if the fullscreen application shows graphical glitches. 2 - off, 1 - on, 2 - auto (on with content type ‘game’)
+        direct_scanout = 0, -- Enables direct scanout. Direct scanout attempts to reduce lag when there is only one fullscreen application on a screen (e.g. game). It is also recommended to set this to false if the fullscreen application shows graphical glitches. 2 - off, 1 - on, 2 - auto (on with content type ‘game’)
+        -- Set this to 0 to avoid breakage in HDR. If left on, direct_scanout will disrupt hyprland's ability to properly facilitate hdr swapchain. A lot of heartache went into learning about this
         -- cm_fs_passthrough = 2,  -- 1 = always pass through color settings, 2 = HDR only
-        cm_sdr_eotf = 2, -- Default transfer function for displaying SDR apps. 0 - Default (currently gamma22), 1 - Treat unspecified as Gamma 2.2, 2 - Treat unspecified and sRGB as Gamma 2.2, 3 - Treat unspecified as sRGB (previous default)
+        cm_sdr_eotf = "gamma22force", -- Default transfer function for displaying SDR apps. "default" - Use default value (sRGB), "gamma22" - Treat unspecified as Gamma 2.2, "gamma22force" - Treat unspecified and sRGB as Gamma 2.2, "srgb" - Treat unspecified as sRGB
         cm_auto_hdr = 2, -- Auto-switch to HDR in fullscreen when needed. 0 - off, 1 - switch to cm, hdr, 2 - switch to cm, hdredid
         -- cm_auto_hdr = 0 if games refuse to play nice and appear with washed out colors
     }
