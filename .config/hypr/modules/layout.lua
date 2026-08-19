90fe55e5baf0fe9f2188b270f2b4baf99d93398b
maya <mayaephemera@proton.me>
2026-08-19 12:31:10 -0600

    change default mfact to .60 for proper discord/browser aspect ratio split in master layout

--- .config/hypr/modules/layout.lua
+++ .config/hypr/modules/layout.lua
@@ -33,7 +33,7 @@ hl.config({
 
 hl.config({
   master = {
-      mfact                        = 0.55,
+      mfact                        = 0.60,
       new_status                   = "slave",
       new_on_top                   = true,
       orientation                  = "left",
@@ -41,13 +41,13 @@ hl.config({
 })
 
 hl.config({
-  scrolling = {
-     fullscreen_on_one_column      = true,
-     column_width                  = 0.5,
-     focus_fit_method              = 1,
-     follow_focus                  = true,
-     follow_min_visible            = 0.4,
-     explicit_column_widths        = 0.33, 0.5, 0.667, 1.0,
-     direction                     = "right",
-  },
+    scrolling = {
+        fullscreen_on_one_column = true,
+        column_width = 0.5,
+        focus_fit_method = 1,
+        follow_focus = true,
+        follow_min_visible = 0.4,
+        explicit_column_widths = "0.33, 0.5, 0.667, 1.0",
+        direction = "right",
+    },
 })
